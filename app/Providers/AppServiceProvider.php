e5d85bd9b85cf8df85782504b2b1cd0cbde72cbb
vremes <valtteriremes@outlook.com>
2024-04-13 18:13:49 +0000

    Removed unused use statements.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,7 @@
 
 namespace App\Providers;
 
-use App\Enums\Locale;
-use Illuminate\Support\Facades\App;
-use Illuminate\Support\Facades\View;
+
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
