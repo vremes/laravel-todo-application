4ebe0d9266c86f02e99f1418a55543400541c7c3
vremes <valtteriremes@outlook.com>
2024-04-13 21:22:06 +0000

    Better login and register templates.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,22 +3,24 @@
 @section('title', __('Login'))
 
 @section('content')
-<div class="container">
-    <div>
-        <form action="{{ route('auth.handle_login') }}" method="POST">
-            <div class="mb-3">
-                <label for="name" class="form-label">{{ __('Username') }}</label>
-                <input type="text" name="name" class="form-control" placeholder="{{ __('Enter username') }}" minlength="4" maxlength="48" required>
-            </div>
-            <div class="mb-3">
-                <label for="password" class="form-label">{{ __('Password') }}</label>
-                <input type="password" name="password" class="form-control" placeholder="{{ __('Password') }}" minlength="8" maxlength="70" required>
-            </div>
-            <div>
-                @csrf()
-                <button type="submit" class="btn btn-outline-primary">{{ __('Login') }}</button>
-            </div>
-        </form>
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-4">
+            <form action="{{ route('auth.handle_login') }}" method="POST">
+                <div class="mb-3">
+                    <label for="name" class="form-label">{{ __('Username') }}</label>
+                    <input type="text" name="name" class="form-control" placeholder="{{ __('Enter username') }}" minlength="4" maxlength="48" required>
+                </div>
+                <div class="mb-3">
+                    <label for="password" class="form-label">{{ __('Password') }}</label>
+                    <input type="password" name="password" class="form-control" placeholder="{{ __('Password') }}" minlength="8" maxlength="70" required>
+                </div>
+                <div class="d-flex justify-content-center">
+                    @csrf()
+                    <button type="submit" class="btn btn-outline-primary">{{ __('Login') }}</button>
+                </div>
+            </form>
+        </div>
     </div>
 </div>
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,30 +3,32 @@
 @section('title', __('Register'))
 
 @section('content')
-<div class="container">
-    <div>
-        <form action="{{ route('auth.handle_register') }}" method="POST">
-            <div class="mb-3">
-                <label for="name" class="form-label">{{ __('Username') }}</label>
-                <input type="text" name="name" class="form-control" placeholder="Enter username" minlength="4" maxlength="48" required>
-            </div>
-            <div class="mb-3">
-                <label for="email" class="form-label">{{ __('Email address (optional)') }}</label>
-                <input type="email" name="email" class="form-control" placeholder="Enter email address (optional)" maxlength="255">
-            </div>
-            <div class="mb-3">
-                <label for="password" class="form-label">{{ __('Password') }}</label>
-                <input type="password" name="password" class="form-control" placeholder="{{ __('Enter password') }}" minlength="8" maxlength="70" required>
-            </div>
-            <div class="mb-3">
-                <label for="password_confirmation" class="form-label">{{ __('Confirm password') }}</label>
-                <input type="password" name="password_confirmation" class="form-control" placeholder="{{ __('Confirm password') }}" minlength="8" maxlength="70" required>
-            </div>
-            <div>
-                @csrf()
-                <button type="submit" class="btn btn-outline-primary">{{ __('Register') }}</button>
-            </div>
-        </form>
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-4">
+            <form action="{{ route('auth.handle_register') }}" method="POST">
+                <div class="mb-3">
+                    <label for="name" class="form-label">{{ __('Username') }}</label>
+                    <input type="text" name="name" class="form-control" placeholder="Enter username" minlength="4" maxlength="48" required>
+                </div>
+                <div class="mb-3">
+                    <label for="email" class="form-label">{{ __('Email address (optional)') }}</label>
+                    <input type="email" name="email" class="form-control" placeholder="Enter email address (optional)" maxlength="255">
+                </div>
+                <div class="mb-3">
+                    <label for="password" class="form-label">{{ __('Password') }}</label>
+                    <input type="password" name="password" class="form-control" placeholder="{{ __('Enter password') }}" minlength="8" maxlength="70" required>
+                </div>
+                <div class="mb-3">
+                    <label for="password_confirmation" class="form-label">{{ __('Confirm password') }}</label>
+                    <input type="password" name="password_confirmation" class="form-control" placeholder="{{ __('Confirm password') }}" minlength="8" maxlength="70" required>
+                </div>
+                <div class="d-flex justify-content-center">
+                    @csrf()
+                    <button type="submit" class="btn btn-outline-primary">{{ __('Register') }}</button>
+                </div>
+            </form>
+        </div>
     </div>
 </div>
 @endsection
