d176d1b02142c834ee55d68cb243a1fd049df956
vremes <valtteriremes@outlook.com>
2024-04-13 21:54:21 +0000

    Added HasUuids trait to Todo.

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -2,11 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Todo extends Model
 {
+    use HasUuids;
+
     /**
      * The attributes that are mass assignable.
      *
