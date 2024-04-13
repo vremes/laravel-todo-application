44fd851aa64988e9340241724d473c45162d8dc4
vremes <valtteriremes@outlook.com>
2024-04-13 16:34:46 +0000

    Registration localization.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,24 +7,24 @@
     <div>
         <form action="{{ route('auth.handle_register') }}" method="POST">
             <div class="mb-3">
-                <label for="name" class="form-label">Username</label>
+                <label for="name" class="form-label">{{ __('Username') }}</label>
                 <input type="text" name="name" class="form-control" placeholder="Enter username" minlength="4" maxlength="48" required>
             </div>
             <div class="mb-3">
-                <label for="email" class="form-label">Email address (optional)</label>
+                <label for="email" class="form-label">{{ __('Email address (optional)') }}</label>
                 <input type="email" name="email" class="form-control" placeholder="Enter email address (optional)" maxlength="255">
             </div>
             <div class="mb-3">
-                <label for="password" class="form-label">Password</label>
-                <input type="password" name="password" class="form-control" placeholder="Enter password" minlength="8" maxlength="70" required>
+                <label for="password" class="form-label">{{ __('Password') }}</label>
+                <input type="password" name="password" class="form-control" placeholder="{{ __('Enter password') }}" minlength="8" maxlength="70" required>
             </div>
             <div class="mb-3">
-                <label for="password_confirmation" class="form-label">Password</label>
-                <input type="password" name="password_confirmation" class="form-control" placeholder="Confirm password" minlength="8" maxlength="70" required>
+                <label for="password_confirmation" class="form-label">{{ __('Confirm password') }}</label>
+                <input type="password" name="password_confirmation" class="form-control" placeholder="{{ __('Confirm password') }}" minlength="8" maxlength="70" required>
             </div>
             <div>
                 @csrf()
-                <button type="submit" class="btn btn-outline-primary">Register</button>
+                <button type="submit" class="btn btn-outline-primary">{{ __('Register') }}</button>
             </div>
         </form>
     </div>
