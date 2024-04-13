2b31ff02d511b545cf7b22443720dd089b0557cf
vremes <valtteriremes@outlook.com>
2024-04-13 16:07:14 +0000

    Make Todo title required.

--- database/migrations/2024_04_13_123952_create_todos_table.php
+++ database/migrations/2024_04_13_123952_create_todos_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('todos', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->string('title')->nullable();
+            $table->string('title');
             $table->string('description')->nullable();
             $table->bigInteger('user_id')->unsigned();
             $table->foreign('user_id')
